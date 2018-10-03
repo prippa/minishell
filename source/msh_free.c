1d1cb6d7d808e2b579e6702beec74ca88825d453
prippa <pavelrippa@gmail.com>
2018-10-03 23:39:24 +0300

    update

--- source/msh_free.c
+++ source/msh_free.c
@@ -25,4 +25,5 @@ void		msh_free(t_msh *msh)
 {
 	ft_lst2del(&msh->env_start, &msh->env_end, msh_del_env_list);
 	ft_lst2del(&msh->cmd_start, &msh->cmd_end, msh_del_cmd_list);
+	ft_strdel(&msh->line);
 }
